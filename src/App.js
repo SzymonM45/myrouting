2dad94a6785aa85cc659b4c641567d78f8c3a096
Szymon <edbsiedlce@gmail.com>
2024-08-23 19:39:52 +0200

    Nowy plik

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Link, NavLink, Route, Routes, } from 'react-router-dom';
+import Test from './Test';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Home = () => {
+  return(
+<h1>Strona startowa</h1>
+  )
+}
+
+ const News = () => <h1>Aktualności</h1>
+ 
+ const Contact = () => <h1>Kontakt</h1>
+
+ const ErrorPage = () => <h1>Strona nie istnieje</h1>
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+      <div>
+        <header>
+          <nav>
+            <ul>
+              {/* <li><a href="/">Start</a></li>
+              <li><a href="/news">Aktualności</a></li>
+              <li><a href="/contact">Kontakt</a></li> */}
+              <li><NavLink  to="/" className={({ isActive }) => isActive ? "home_selected" : ''}>Start</NavLink></li>
+
+              <li><NavLink to="/news" className={({ isActive }) => isActive ? 
+              "news_selected" : ''}>Aktualności</NavLink></li>
+              <li><NavLink to="/contact" className={({ isActive }) => isActive ? 
+              "contact_selected" : ''}>Kontakt</NavLink></li>
+              
+            </ul>
+          </nav>
+        </header>
+        <section>
+        <Routes>
+        <Route path="/"  element={<Home/>}/>
+        <Route path="/news" element={<News/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="*" element={<ErrorPage/>}/>
+        </Routes>
+        </section>
+
+        <Test/>
+
+       
+      </div>
+     
+      </Router>
+
+    );
+  }
+
 }
 
 export default App;
